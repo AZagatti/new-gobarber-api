4a7785ee934bb5f7752caf71a132227f7972ff1f
azagatti <andre.zagatti1@gmail.com>
2020-05-08 16:01:47 -0300

    feat: create list availability in day and month for provider

---
+++ src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderMonthAvailabilityService from '@modules/appointments/services/ListProviderMonthAvailabilityService';
+
+export default class ProviderMonthAvailabilityController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const { provider_id } = req.params;
+    const { month, year } = req.body;
+
+    const listProviderMonthAvailabilityService = container.resolve(
+      ListProviderMonthAvailabilityService,
+    );
+
+    const availability = await listProviderMonthAvailabilityService.execute({
+      month,
+      provider_id,
+      year,
+    });
+
+    return res.json(availability);
+  }
+}
