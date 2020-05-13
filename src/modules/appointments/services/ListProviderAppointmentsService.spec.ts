adb53478c399d903367c94ddc24e4be59e40eec1
azagatti <andre.zagatti1@gmail.com>
2020-05-13 08:14:22 -0300

    feat: added route to list appointments for a specific provider

--- src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -14,23 +14,27 @@ describe('ListProviderAppointments', () => {
   });
 
   it('should be able list the appointments on a specific day', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
     const appointment1 = await fakeAppointmentsRepository.create({
       provider_id: 'provider',
       user_id: 'user',
-      date: new Date(2020, 4, 20, 14, 0, 0),
+      date: new Date(2020, 4, 13, 14, 0, 0),
     });
 
     const appointment2 = await fakeAppointmentsRepository.create({
       provider_id: 'provider',
       user_id: 'user',
-      date: new Date(2020, 4, 20, 15, 0, 0),
+      date: new Date(2020, 4, 13, 15, 0, 0),
     });
 
     const appointments = await listProviderAppointmentsService.execute({
-      provider_id: 'user',
+      provider_id: 'provider',
       year: 2020,
       month: 5,
-      day: 20,
+      day: 13,
     });
 
     expect(appointments).toEqual(
