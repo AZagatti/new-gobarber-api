4d82badf03e7cf529d17c2c00bf6dab9e175e4e4
azagatti <andre.zagatti1@gmail.com>
2020-05-23 22:52:27 -0300

    fix: ratelimit img

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -18,9 +18,9 @@ const PORT = 3333;
 const app = express();
 app.use(cors());
 
-app.use(rateLimiter);
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(rateLimiter);
 app.use(routes);
 
 app.use(errors());
